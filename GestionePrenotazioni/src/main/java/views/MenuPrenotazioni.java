84fd01bdf73a7a1ae811650509067509c390b1e6
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-07-20 21:10:16 +0200

    dichiarazione delle funzioni di MenuPrenotazioni

--- GestionePrenotazioni/src/main/java/views/MenuPrenotazioni.java
+++ GestionePrenotazioni/src/main/java/views/MenuPrenotazioni.java
@@ -3,4 +3,33 @@ package views;
 import javax.swing.*;
 
 public class MenuPrenotazioni extends JPanel {
+
+    private JTable tabellaPrenotazioni;
+    private JPanel pnlTabellaPrenotazioni;
+    private JToolBar toolBar;
+
+    public MenuPrenotazioni() {
+
+        initUiComponents();
+
+        setupToolbar();
+
+        setupTable();
+    }
+
+    // Inizializza le componenti grafiche
+    private void initUiComponents(){
+
+    }
+
+    // Setup della tabella delle prenotazioni
+    private void setupTable(){
+
+    }
+
+    // Setup toolbar
+    private void setupToolbar(){
+
+    }
+
 }
