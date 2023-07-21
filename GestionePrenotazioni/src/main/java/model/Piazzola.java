e87b55990d2b9691e1f33d212d697dc4f60fe6d3
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-07-21 15:48:39 +0200

    implementata classe Piazzola

--- GestionePrenotazioni/src/main/java/model/Piazzola.java
+++ GestionePrenotazioni/src/main/java/model/Piazzola.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Piazzola {
+    private String nome;
+
+    public Piazzola(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
 }
