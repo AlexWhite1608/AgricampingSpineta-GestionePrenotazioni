364d8436c6e60b02d74a7b86cecef87adccec026
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-07-21 16:17:39 +0200

    modificata generazione id prenotazione

--- GestionePrenotazioni/src/main/java/utils/IDGenerator.java
+++ GestionePrenotazioni/src/main/java/utils/IDGenerator.java
@@ -1,19 +1,16 @@
 package utils;
 
-import java.security.SecureRandom;
+import java.util.Calendar;
+import java.util.UUID;
 
 public class IDGenerator {
-    private static final String ALLOWED_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private static SecureRandom random = new SecureRandom();
-    private static final int length = 6;
 
     public static String generateRandomId() {
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int randomIndex = random.nextInt(ALLOWED_CHARACTERS.length());
-            char randomChar = ALLOWED_CHARACTERS.charAt(randomIndex);
-            sb.append(randomChar);
-        }
-        return sb.toString();
+        String uniqueID = UUID.randomUUID().toString();
+
+        String currentYear = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+        String currentDay = String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_YEAR));
+
+        return currentYear + "-" + currentDay + uniqueID;
     }
 }
