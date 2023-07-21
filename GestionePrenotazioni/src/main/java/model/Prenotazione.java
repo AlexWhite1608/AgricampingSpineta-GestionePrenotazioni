733d27922852bb83f658eae734f1e27be7e19df9
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-07-21 16:19:05 +0200

    inizializzata classe Prenotazione

--- GestionePrenotazioni/src/main/java/model/Prenotazione.java
+++ GestionePrenotazioni/src/main/java/model/Prenotazione.java
@@ -1,4 +1,65 @@
 package model;
 
+import utils.IDGenerator;
+
+import java.util.Date;
+
 public class Prenotazione {
+
+    private final String id = IDGenerator.generateRandomId();
+    private Piazzola piazzola;
+    private Date dataArrivo;
+    private Date dataPartenza;
+    private String nome;
+    private String acconto = "";
+    private String info = "";
+    private String telefono = "";
+    private String email = "";
+
+    public Prenotazione(Piazzola piazzola, Date dataArrivo, Date dataPartenza, String nome, String acconto, String info, String telefono, String email) {
+        this.piazzola = piazzola;
+        this.dataArrivo = dataArrivo;
+        this.dataPartenza = dataPartenza;
+        this.nome = nome;
+        this.acconto = acconto;
+        this.info = info;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Piazzola getPiazzola() {
+        return piazzola;
+    }
+
+    public Date getDataArrivo() {
+        return dataArrivo;
+    }
+
+    public Date getDataPartenza() {
+        return dataPartenza;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAcconto() {
+        return acconto;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
