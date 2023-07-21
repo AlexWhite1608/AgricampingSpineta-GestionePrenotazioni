15dabd77b0c78ea78750666bbde071ad65d91e8f
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-07-21 16:09:03 +0200

    implementato generatore di id random per le prenotazioni

---
+++ GestionePrenotazioni/src/main/java/utils/IDGenerator.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.security.SecureRandom;
+
+public class IDGenerator {
+    private static final String ALLOWED_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static SecureRandom random = new SecureRandom();
+    private static final int length = 6;
+
+    public static String generateRandomId() {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(ALLOWED_CHARACTERS.length());
+            char randomChar = ALLOWED_CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
+}
