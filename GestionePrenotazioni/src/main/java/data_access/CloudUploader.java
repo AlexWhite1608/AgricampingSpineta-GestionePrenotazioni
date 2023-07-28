a2bb1c852d0316dff20a2937a06cde8174ddfb89
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-07-28 15:37:40 +0200

    funziona il caricamento su drive di prova

--- GestionePrenotazioni/src/main/java/data_access/CloudUploader.java
+++ GestionePrenotazioni/src/main/java/data_access/CloudUploader.java
@@ -1,4 +1,89 @@
 package data_access;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 public class CloudUploader {
+    private static final String APPLICATION_NAME = "ProvaCampeggio";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String CREDENTIALS_FILE_PATH = "/prova_credenziali_drive.json";
+    private static final String TOKENS_DIRECTORY_PATH = "/home/alessandro/Dev/Java/GestionePrenotazioni/GestionePrenotazioni/tokens";   //FIXME: crea cartella da qualche parte!
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Create a new file on Drive
+        InputStream fileStream = Objects.requireNonNull(CloudUploader.class.getResource("/database.db")).openStream();
+        java.io.File tempFile = createTempFile(fileStream, "database", ".db");
+        File fileMetadata = new File();
+        fileMetadata.setName("database.db");
+
+        FileContent mediaContent = new FileContent("application/sqlite", tempFile);
+        File uploadedFile = service.files().create(fileMetadata, mediaContent).setFields("id").execute();
+
+        System.out.println("File ID: " + uploadedFile.getId());
+
+        // Delete the temporary file
+        tempFile.delete();
+    }
+
+    private static Credential getCredentials(final HttpTransport httpTransport) throws IOException {
+        InputStream in = Objects.requireNonNull(CloudUploader.class.getResource(CREDENTIALS_FILE_PATH)).openStream();
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    private static java.io.File createTempFile(InputStream inputStream, String prefix, String suffix) throws IOException {
+        java.io.File tempFile = java.io.File.createTempFile(prefix, suffix);
+        tempFile.deleteOnExit();
+
+        try (OutputStream outputStream = new FileOutputStream(tempFile)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        }
+
+        return tempFile;
+    }
 }
