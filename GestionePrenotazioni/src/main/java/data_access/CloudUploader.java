22b75b064766963ab80b4ec60a963d7894ca5419
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-07-28 16:02:20 +0200

    generalizzato percorso dei tokens

--- GestionePrenotazioni/src/main/java/data_access/CloudUploader.java
+++ GestionePrenotazioni/src/main/java/data_access/CloudUploader.java
@@ -23,6 +23,7 @@ import com.google.api.services.drive.model.FileList;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystems;
 import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
@@ -33,18 +34,25 @@ public class CloudUploader {
     private static final String APPLICATION_NAME = "ProvaCampeggio";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String CREDENTIALS_FILE_PATH = "/prova_credenziali_drive.json";
-    private static final String TOKENS_DIRECTORY_PATH = "/home/alessandro/Dev/Java/GestionePrenotazioni/GestionePrenotazioni/tokens";   //FIXME: crea cartella da qualche parte!
+    private static final String TOKENS_DIRECTORY_PATH = System.getProperty("user.home") + FileSystems.getDefault().getSeparator() + "GestionePrenotazioni" + FileSystems.getDefault().getSeparator() + "tokens";    //FIXME: crea cartella da qualche parte!
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
 
-    public static void main(String[] args) throws IOException, GeneralSecurityException {
-        // Build a new authorized API client service.
+    //FIXME: rimuovi quando funziona e chiama direttamente uploadFile
+    public static void main(String[] args) {
+        try {
+            uploadFile();
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //TODO: se il file è già presente sostituiscilo oppure fai una cartella con il giorno del caricamento?
+    private static void uploadFile() throws IOException, GeneralSecurityException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT)).setApplicationName(APPLICATION_NAME).build();
 
         // Create a new file on Drive
-        InputStream fileStream = Objects.requireNonNull(CloudUploader.class.getResource("/database.db")).openStream();
+        InputStream fileStream = Objects.requireNonNull(CloudUploader.class.getResourceAsStream("/database.db"));
         java.io.File tempFile = createTempFile(fileStream, "database", ".db");
         File fileMetadata = new File();
         fileMetadata.setName("database.db");
