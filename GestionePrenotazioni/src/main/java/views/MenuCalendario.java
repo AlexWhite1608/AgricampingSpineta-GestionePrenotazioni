a40e53c02980ea9a80c8965f5c9182ad44e1ed39
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-08-01 10:24:03 +0200

    inizializzato MenuCalendario

--- GestionePrenotazioni/src/main/java/views/MenuCalendario.java
+++ GestionePrenotazioni/src/main/java/views/MenuCalendario.java
@@ -1,6 +1,36 @@
 package views;
 
+import controllers.TableCalendarioController;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuCalendario extends JPanel {
+
+    // Controller della tabella
+    TableCalendarioController tableCalendarioController;
+
+    private JPanel mainPanelCalendario;
+    private JTable tblCalendario;
+
+    public MenuCalendario() {
+        tableCalendarioController = new TableCalendarioController();
+
+        createUIComponents();
+        setupTable();
+
+        setLayout(new BorderLayout());
+        add(mainPanelCalendario, BorderLayout.CENTER);
+        setVisible(true);
+    }
+
+    // Inizializzazione degli elementi di UI
+    private void createUIComponents() {
+
+    }
+
+    // Setup tabella calendario
+    private void setupTable() {
+
+    }
 }
