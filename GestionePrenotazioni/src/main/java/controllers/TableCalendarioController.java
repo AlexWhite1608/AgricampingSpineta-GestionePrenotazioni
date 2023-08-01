c5bb5c93300319caf56f1f55465d886f8d1a2c52
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-08-01 10:57:53 +0200

    inizializzato i metodi di TableCalendarioController

--- GestionePrenotazioni/src/main/java/controllers/TableCalendarioController.java
+++ GestionePrenotazioni/src/main/java/controllers/TableCalendarioController.java
@@ -1,4 +1,54 @@
 package controllers;
 
+import data_access.Gateway;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+import java.sql.SQLException;
+
 public class TableCalendarioController {
+    private JTable tabellaCalendario;
+    private final Gateway gateway;
+
+    public TableCalendarioController(JTable tabellaCalendario) {
+        this.tabellaCalendario = tabellaCalendario;
+        this.gateway = new Gateway();
+    }
+
+    // Imposta la visualizzazione iniziale della tabella Calendario
+    public JTable initView() {
+        return null;
+    }
+
+    // Ricarica la tabella Calendario per aggiornamenti
+    public void refreshTable(JTable tabellaCalendario) {
+
+    }
+
+    // Imposta il renderer per le celle
+    public DefaultTableCellRenderer createCellRenderer() {
+        return new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+                return c;
+            }
+        };
+    }
+
+
+    // Imposta il renderer per l'header
+    public DefaultTableCellRenderer createHeaderRenderer() {
+        return new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+                return c;
+            }
+        };
+    }
+
 }
--- GestionePrenotazioni/src/main/java/views/MenuCalendario.java
+++ GestionePrenotazioni/src/main/java/views/MenuCalendario.java
@@ -11,10 +11,10 @@ public class MenuCalendario extends JPanel {
     TableCalendarioController tableCalendarioController;
 
     private JPanel mainPanelCalendario;
-    private JTable tblCalendario;
+    private JTable tabellaCalendario;
 
     public MenuCalendario() {
-        tableCalendarioController = new TableCalendarioController();
+        tableCalendarioController = new TableCalendarioController(tabellaCalendario);
 
         createUIComponents();
         setupTable();
@@ -27,10 +27,25 @@ public class MenuCalendario extends JPanel {
     // Inizializzazione degli elementi di UI
     private void createUIComponents() {
 
+        // Main panel
+        mainPanelCalendario = new JPanel();
+        mainPanelCalendario.setLayout(new BorderLayout());
+
+        //TODO: istanzia la tabella col controller
     }
 
     // Setup tabella calendario
     private void setupTable() {
+        tabellaCalendario.getTableHeader().setReorderingAllowed(false);
+        tabellaCalendario.setCellSelectionEnabled(false);
+        tabellaCalendario.setRowSelectionAllowed(true);
+        tabellaCalendario.setDefaultEditor(Object.class, null);
 
+        // Dimensione righe tabella
+        int rowHeight = 40;
+        tabellaCalendario.setRowHeight(rowHeight);
+
+        JScrollPane scrollPane = new JScrollPane(tabellaCalendario);
+        mainPanelCalendario.add(scrollPane, BorderLayout.CENTER);
     }
 }
