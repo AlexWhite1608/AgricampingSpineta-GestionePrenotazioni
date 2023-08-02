a0b4b8a860884a91975a5138e6625aefad51041e
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-08-02 15:47:05 +0200

    spostato metodo che adatta larghezza colonne nel manager del calendario

--- GestionePrenotazioni/src/main/java/controllers/TableCalendarioController.java
+++ GestionePrenotazioni/src/main/java/controllers/TableCalendarioController.java
@@ -3,9 +3,7 @@ package controllers;
 import data_access.Gateway;
 
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.*;
 import java.awt.*;
 import java.sql.SQLException;
 import java.util.List;
@@ -87,10 +85,6 @@ public class TableCalendarioController {
 //
 //    }
 
-    // Ricarica le piazzole
-    public void refreshPiazzole(JTable tabellaCalendario) {
-
-    }
 
     // Imposta il renderer per le celle
     public DefaultTableCellRenderer createCellRenderer() {
@@ -127,6 +121,29 @@ public class TableCalendarioController {
                 return c;
             }
         };
+    }
+
+    // Adatta la larghezza delle colonne
+    public void adaptColumnsWidthToHeader() {
+        int padding = 20; // Aggiungi un valore di padding desiderato
+        for (int column = 0; column < tabellaCalendario.getColumnCount(); column++) {
+            TableColumn tableColumn = tabellaCalendario.getColumnModel().getColumn(column);
+            TableCellRenderer headerRenderer = tabellaCalendario.getTableHeader().getDefaultRenderer();
+            Object headerValue = tableColumn.getHeaderValue();
+            Component headerComp = headerRenderer.getTableCellRendererComponent(tabellaCalendario, headerValue, false, false, 0, column);
+            int headerWidth = headerComp.getPreferredSize().width;
+            int maxCellWidth = 0;
+
+            for (int row = 0; row < tabellaCalendario.getRowCount(); row++) {
+                TableCellRenderer cellRenderer = tabellaCalendario.getCellRenderer(row, column);
+                Component cellComp = tabellaCalendario.prepareRenderer(cellRenderer, row, column);
+                int cellWidth = cellComp.getPreferredSize().width + tabellaCalendario.getIntercellSpacing().width;
+                maxCellWidth = Math.max(maxCellWidth, cellWidth);
+            }
+
+            int preferredWidth = Math.max(headerWidth, maxCellWidth) + padding; // Aggiungi il padding
+            tableColumn.setPreferredWidth(preferredWidth);
+        }
     }
 
 }
--- GestionePrenotazioni/src/main/java/views/MenuCalendario.java
+++ GestionePrenotazioni/src/main/java/views/MenuCalendario.java
@@ -53,28 +53,10 @@ public class MenuCalendario extends JPanel {
         tabellaCalendario.setDefaultEditor(Object.class, null);
         tabellaCalendario.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-        int padding = 20; // Aggiungi un valore di padding desiderato
-        for (int column = 0; column < tabellaCalendario.getColumnCount(); column++) {
-            TableColumn tableColumn = tabellaCalendario.getColumnModel().getColumn(column);
-            TableCellRenderer headerRenderer = tabellaCalendario.getTableHeader().getDefaultRenderer();
-            Object headerValue = tableColumn.getHeaderValue();
-            Component headerComp = headerRenderer.getTableCellRendererComponent(tabellaCalendario, headerValue, false, false, 0, column);
-            int headerWidth = headerComp.getPreferredSize().width;
-            int maxCellWidth = 0;
+        // Adatta la larghezza delle colonne!
+        tableCalendarioController.adaptColumnsWidthToHeader();
 
-            for (int row = 0; row < tabellaCalendario.getRowCount(); row++) {
-                TableCellRenderer cellRenderer = tabellaCalendario.getCellRenderer(row, column);
-                Component cellComp = tabellaCalendario.prepareRenderer(cellRenderer, row, column);
-                int cellWidth = cellComp.getPreferredSize().width + tabellaCalendario.getIntercellSpacing().width;
-                maxCellWidth = Math.max(maxCellWidth, cellWidth);
-            }
-
-            int preferredWidth = Math.max(headerWidth, maxCellWidth) + padding; // Aggiungi il padding
-            tableColumn.setPreferredWidth(preferredWidth);
-        }
-
-
-        // Renderer per il testo delle celle
+        //TODO: Renderer per il testo delle celle
 
         // Renderer per l'header
         DefaultTableCellRenderer headerRenderer = tableCalendarioController.createHeaderRenderer();
