3cc7de54b2e658f21d03d74eb7e117ae195bdfc6
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-08-03 14:22:59 +0200

    impostati i colori delle piazzole

--- GestionePrenotazioni/src/main/java/renderers_calendario/CalendarioCellRenderer.java
+++ GestionePrenotazioni/src/main/java/renderers_calendario/CalendarioCellRenderer.java
@@ -19,24 +19,43 @@ public class CalendarioCellRenderer extends DefaultTableCellRenderer {
         setHorizontalAlignment(HORIZONTAL);
         setVerticalAlignment(CENTER);
 
-        // La prima colonna (diversa dalle piazzole) deve essere sempre evidenziata
-        if (column == 1) {
-            c.setBackground(TableConstants.CALENDARIO_FIRST_DAY_COLOR);
-        } else if (table.getColumnName(column).contains("(S)") || table.getColumnName(column).contains("(D)")){
-            c.setBackground(TableConstants.CALENDARIO_WEEKEND_COLOR);
-        } else {
-            c.setBackground(table.getBackground());
-        }
+        // Imposta il colore e il bordo per le piazzole
+        setColorPiazzole(table, value, column, c);
 
-        //TODO: colorare in modo diverso la colonna delle piazzole
+        //TODO: colorazione delle celle per i giorni prenotati
+
+        return c;
+    }
+
+    // Imposta il colore e il bordo per le piazzole
+    private void setColorPiazzole(JTable table, Object value, int column, Component c) {
         if (column == 0) {
-            c.setBackground(table.getBackground());
+            if (value.toString().contains("StarBox")) {
+                c.setBackground(TableConstants.CALENDARIO_STARSBOX_COLOR);
+            } else if (value.toString().contains("Margherita")) {
+                c.setBackground(TableConstants.CALENDARIO_MARGHERITA_COLOR);
+            } else if (value.toString().contains("G")) {
+                c.setBackground(TableConstants.CALENDARIO_G_COLOR);
+            } else if (value.toString().contains("BBQ")) {
+                c.setBackground(TableConstants.CALENDARIO_BBQ_COLOR);
+            } else {
+                c.setBackground(table.getBackground());
+            }
+
+            // Imposta il bordo per la colonna delle Piazzole
             ((JLabel) c).setBorder(BorderFactory.createMatteBorder(0, 0, 0, TableConstants.SEPARATOR_BORDER_WIDTH, Color.BLACK));
         } else {
             ((JLabel) c).setBorder(null);
-        }
 
-        return c;
+            // La colonna della data odierna deve essere sempre evidenziata
+            if (column == 1) {
+                c.setBackground(TableConstants.CALENDARIO_FIRST_DAY_COLOR);
+            } else if (table.getColumnName(column).contains("(S)") || table.getColumnName(column).contains("(D)")) {
+                c.setBackground(TableConstants.CALENDARIO_WEEKEND_COLOR);
+            } else {
+                c.setBackground(table.getBackground());
+            }
+        }
     }
 
     // Calcola l'altezza delle righe
--- GestionePrenotazioni/src/main/java/utils/TableConstants.java
+++ GestionePrenotazioni/src/main/java/utils/TableConstants.java
@@ -7,12 +7,18 @@ public class TableConstants {
     public static final Font CELL_FONT = new Font("Segoe UI", Font.PLAIN, 14);
     public static final Font HEADER_FONT_PRENOTAZIONI = new Font("Segoe UI", Font.BOLD, 16);
     public static final Font HEADER_FONT_CALENDARIO = new Font("Segoe UI", Font.BOLD, 12);
+
     public static final Color ALTERNATE_CELL_COLOR = new Color(220, 232, 234);
     public static final Color SELECTION_COLOR = new Color(255, 255, 102);
     public static final Color ACCONTO_SALDATO_COLOR = new Color(14, 129, 60);
-    public static final Color CALENDARIO_FIRST_DAY_COLOR = new Color(52, 201, 235);
-    public static final Color CALENDARIO_WEEKEND_COLOR = Color.lightGray;
+    public static final Color CALENDARIO_FIRST_DAY_COLOR = new Color(166, 197, 245);
+    public static final Color CALENDARIO_WEEKEND_COLOR = new Color(208, 208, 208);
+    public static final Color CALENDARIO_STARSBOX_COLOR = new Color(245, 235, 169);
+    public static final Color CALENDARIO_G_COLOR = new Color(189, 255, 199);
+    public static final Color CALENDARIO_MARGHERITA_COLOR = new Color(222, 204, 252);
+    public static final Color CALENDARIO_BBQ_COLOR = new Color(252, 157, 157);
     public static final Color BORDER_CELL_SELECTED = Color.blue;
+
     public static final int COLUMNS_WIDTH_CALENDARIO = 45;
     public static final int SEPARATOR_BORDER_WIDTH = 3;
     public static final int DAYS_BEFORE_DELETE = 4;
