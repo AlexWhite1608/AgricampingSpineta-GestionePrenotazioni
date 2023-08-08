a763ca12fef3ccb09a17841d46643a78ad887b75
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-08-08 21:33:52 +0200

    modifiche ai colori

--- GestionePrenotazioni/src/main/java/utils/TableConstants.java
+++ GestionePrenotazioni/src/main/java/utils/TableConstants.java
@@ -14,12 +14,11 @@ public class TableConstants {
     public static final Color CALENDARIO_FIRST_DAY_COLOR = new Color(166, 197, 245);
     public static final Color CALENDARIO_WEEKEND_COLOR = new Color(208, 208, 208);
     public static final Color CALENDARIO_STARSBOX_COLOR = new Color(245, 237, 86);
-    public static final Color CALENDARIO_G_COLOR = new Color(189, 255, 199);
-    public static final Color CALENDARIO_MARGHERITA_COLOR = new Color(222, 204, 252);
+    public static final Color CALENDARIO_G_COLOR = new Color(117, 232, 104);
+    public static final Color CALENDARIO_MARGHERITA_COLOR = new Color(252, 163, 53);
     public static final Color CALENDARIO_BBQ_COLOR = new Color(252, 157, 157);
     public static final Color BORDER_CELL_SELECTED = Color.blue;
     public static final Color CALENDARIO_PRENOTAZIONE_COLOR =  new Color(54, 70, 255);
-    public static final Color CALENDARIO_CELL_BORDER_COLOR = Color.WHITE;
 
     public static final int COLUMNS_WIDTH_CALENDARIO = 45;
     public static final int SEPARATOR_BORDER_WIDTH = 3;
