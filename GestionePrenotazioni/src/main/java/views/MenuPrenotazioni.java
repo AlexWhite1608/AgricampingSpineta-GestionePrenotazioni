e3d0ae02dd34db05257bae8956cee3970e38fcfc
Alessandro Bianco <alessandro.bianco1608@gmail.com>
2023-08-25 16:01:46 +0200

    risolto errore dei datepicker

--- GestionePrenotazioni/src/main/java/views/MenuPrenotazioni.java
+++ GestionePrenotazioni/src/main/java/views/MenuPrenotazioni.java
@@ -584,14 +584,16 @@ public class MenuPrenotazioni extends JPanel implements StopTableEditObservers {
             LocalDate partenza = dateChangeEvent.getNewDate();
             LocalDate arrivo = datePickerArrivo.getDate();
 
-            ControllerDatePrenotazioni.checkOrdineDate(arrivo, arrivo.isAfter(partenza), datePickerPartenza, dialogNuovaPrenotazione);
+            if(arrivo != null && partenza != null)
+                ControllerDatePrenotazioni.checkOrdineDate(arrivo, arrivo.isAfter(partenza), datePickerPartenza, dialogNuovaPrenotazione);
         });
 
         datePickerArrivo.addDateChangeListener((dateChangeEvent) -> {
             LocalDate arrivo = dateChangeEvent.getNewDate();
             LocalDate partenza = datePickerPartenza.getDate();
 
-            ControllerDatePrenotazioni.checkOrdineDate(partenza, partenza.isBefore(arrivo), datePickerArrivo, dialogNuovaPrenotazione);
+            if(partenza != null && arrivo != null)
+                ControllerDatePrenotazioni.checkOrdineDate(partenza, partenza.isBefore(arrivo), datePickerArrivo, dialogNuovaPrenotazione);
         });
 
         // Label scelta piazzola
